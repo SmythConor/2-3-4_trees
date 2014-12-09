1bbe02662ff19c5c06f8c91b90cf07acab9b69de
Conor Smyth <cnrsmyth@gmail.com>
2014-12-09 09:42:06 +0000

    Some more work. Still no idea what I'm doing

--- 234tree.hs
+++ 234tree.hs
@@ -26,14 +26,13 @@ addNode x (Three (a,b) Empty Empty Empty)
 -- Add to two with values
 -- if less than a add to left tree.
 -- if greater than a add to right tree.
+-- Must check for Four Three and Two node
 -- If the left tree is a four tree move middle from four to the two above and
 
 addNode x (Two a l r)
 	| x <= a = case l of (Four (a1,b1,c1) l1 m1 m2 r1)
-																											| x <= a1 -> (Four (a1,b1,c1) (addNode x l1) m1 m2 r1)
-																											| x >= c1 -> (Four (a1,b1,c1) l1 m1 m2 (addNode x r1))
-																											| x <= m1 -> (Four (a1,b1,c1) l1 (addNode x m1) m2 r1)
-																											| otherwise -> (Four (a1,b1,c1) l1 m1 (addNode x m2) r1)
+																											| x <= b1 -> (Three (b1,a) (addNode x (Two a1 l m1)) (Two c1 m2 r1) r)
+																											| otherwise -> (Three (b1,a) l (addNode x (Two c1 m1 m2)) r)
 											 (Three (a1,b1) l1 m1 r1)
 											 																| x <= a1 -> (Four (x,a1,b1) l1 m1 Empty r1)
 											 																| x >= b1 -> (Four (a1,b1,x) l1 m1 Empty r1)
@@ -61,3 +60,4 @@ addNode x (Two a l r)
 myTree2 x = addNode x Empty
 myTree3 x = addNode x (Two 10 Empty Empty)
 myTree4 x = addNode x (Three (8,12) Empty Empty Empty)
+myTree5 x = addNode x (Two 5 3 10)
